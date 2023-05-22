96161b12467d5531e1799e435d7ce07c72ed81db
zoroa <alexis.variot@gmail.com>
2023-05-22 14:50:31 +0200

    [add] init and open window

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,47 @@
 /*
         R&A
 */
+
+#include "main.hpp"
+
+// Init all_c class
+
+void all_c::init_all_c_class(void)
+{
+    mode = sf::VideoMode::getDesktopMode();
+    window.create(mode, "Geometry doush");
+    window.setKeyRepeatEnabled(false);
+}
+
+// Create class
+
+all_c *create_all_c_class(void)
+{
+    all_c *new_all_c = new all_c();
+    new_all_c->init_all_c_class();
+    return new_all_c;
+}
+
+// Loop Game
+
+int all_c::loop_game(void)
+{
+    while (window.isOpen()) {
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+                return 0;
+            }
+        }
+        window.clear(sf::Color::Black);
+        window.display();
+    }
+    return 0;
+}
+
+int main(void)
+{
+    all_c *all = create_all_c_class();
+    all->loop_game();
+    return 0;
+}
--- src/main.hpp
+++ src/main.hpp
@@ -1,3 +1,32 @@
 /*
         R&A
 */
+
+#ifndef main_hpp
+    #define main_hpp
+
+    #include <SFML/Audio.hpp>
+    #include <SFML/Graphics.hpp>
+    #include <SFML/Main.hpp>
+    #include <SFML/System.hpp>
+    #include <SFML/Window.hpp>
+    #include <cmath>
+    #include <dirent.h>
+    #include <string>
+    #include <iterator>
+    #include <list>
+
+    class all_c {
+        public:
+            // Fonction all_c
+            int loop_game(void);
+            void init_all_c_class(void);
+            //variables
+            sf::RenderWindow window;
+            sf::Event event;
+
+        private:
+            sf::VideoMode mode;
+    };
+
+#endif // !main_hpp
